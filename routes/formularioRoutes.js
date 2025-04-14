bf7ceeb8c4ed1124f80d1015468a7f42783d5943
babii26 <barbaramariarodriguesmpa@gmail.com>
2025-04-14 19:42:54 +0100

    Create formularioRoutes.js

---
+++ routes/formularioRoutes.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import Pergunta from '../models/Pergunta';
+import RespostaFormulario from '../models/RespostaFormulario';
+import Alerta from '../models/Alerta';
+
+const router = express.Router();
