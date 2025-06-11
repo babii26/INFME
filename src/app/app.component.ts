6892e6a2f714662ec19977eb1d8304265f3f6e5a
João Barroso <jtm.barroso@gmail.com>
2025-06-11 10:48:39 +0100

    fix maybe?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,12 @@
 import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
-import { AuthService } from './services/auth.service'; 
-import { NavbarComponent } from './navbar/navbar.component';
-import { FormsModule } from '@angular/forms'; 
-import { CommonModule } from '@angular/common';
-
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterModule ,NavbarComponent, FormsModule],
-  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  constructor(private authService: AuthService, private router: Router) {}
-
-  isHomePage(): boolean {
-    return this.router.url === '/';
-  }
+  title = 'Projecto';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,3 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { PhysicianComponent } from './physician/physician.component';
-import { PatientComponent } from './patient/patient.component';
-import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
-import { AdminComponent } from './admin/admin.component';
 
-export const routes: Routes = [
- { path: '', redirectTo: 'login', pathMatch: 'full' }, //redirect to login
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'physician', component: PhysicianComponent },
-  { path: 'patient', component: PatientComponent },
-  { path: 'questionnaire', component: QuestionnaireComponent },
-  { path: 'admin', component: AdminComponent }
-];
+export const routes: Routes = [];
